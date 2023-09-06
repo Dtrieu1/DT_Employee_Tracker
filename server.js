24a007c128b719bfa0b8889dbd6e99429ac089ef
David <131110984+Dtrieu1@users.noreply.github.com>
2023-09-06 18:34:05 -0500

    updated schema file

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
 
+const PORT = process.env.PORT || 3001;
+
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'department_db'
+  },
+  console.log(`Connected to the department_db database.`)
+);
+
 const questions = [
   {
     type: "list",
@@ -25,3 +37,12 @@ const questions = [
     default: "Not Provided",
   },
 ];
+
+function init(){
+  inquirer.prompt(questions).then((answers) => {
+    
+  });
+}
+
+init();
+
